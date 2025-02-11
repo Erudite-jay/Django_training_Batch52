4021a0d297df4b5d62f2c0bdf6f6d19dc4a0235b
Jay <jaydwivedi.programmer@gmail.com>
2025-02-11 13:14:22 -0500

    Day 2 Session 1

--- mysite/Auth_app/admin.py
+++ mysite/Auth_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+admin.site.register(Contact)
+
+# @admin.register(Contact)
+# class ContactTable(admin.ModelAdmin):
+#     list_display=["id","fullname","email","phone","messsage"]
--- mysite/Auth_app/models.py
+++ mysite/Auth_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    fullname=models.CharField(max_length=100)
+    email=models.EmailField(max_length=40)
+    phone=models.CharField(max_length=10)
+    messsage=models.TextField(max_length=250)
